dc786939ca6babcd09f80aa43c2740c0ae30f72d
Boban Stajic <bobanstajic@gmail.com>
2016-10-22 23:09:25 +0200

    heroku base url

--- app/src/main/java/rs/elfak/bobans/carsharing/api/ApiConstants.java
+++ app/src/main/java/rs/elfak/bobans/carsharing/api/ApiConstants.java
@@ -8,7 +8,8 @@ package rs.elfak.bobans.carsharing.api;
 
 class ApiConstants {
 
-    static final String BASE_URL_LIVE = "http://192.168.1.2:9000/";
+    static final String BASE_URL_LOCAL = "http://192.168.1.2:9000/";
+    static final String BASE_URL_LIVE = "https://car-sharing.herokuapp.com/";
 
     static final String LOGIN = "login";
     static final String REGISTER = "register";
